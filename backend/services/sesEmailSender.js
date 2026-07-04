da06ed71a55ff6d6f9d0302fb5f34e891c7c8834
ahmednaeem1012 <ahmednaeem1012@gmail.com>
2026-07-04 20:55:31 +0500

    Integrations Tab + Ses updates

--- backend/services/sesEmailSender.js
+++ backend/services/sesEmailSender.js
@@ -266,7 +266,7 @@ const sendSesCampaignForOrg = async ({ orgId, fromEmail, templateId, templateDat
   if (!templateId && !subject) throw new Error('subject is required when not using templateId.');
   if (!templateId && !html && !text) throw new Error('At least one of templateId, html or text is required.');
 
-  if (templateId) {
+  if (templateId && !html) {
     const { data: template, error: templateError } = await supabase
       .from('templates')
       .select('*')
